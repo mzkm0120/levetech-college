9eccae2e527347486d0e27a4e54cc6ee8baf7cb5
mzkm0120 <mzkm0120@gmail.com>
2025-01-16 00:45:59 +0900

    curriculum 8-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
 
@@ -19,9 +19,9 @@ class PostController extends Controller
         return view('posts.show')->with(['post' => $post]);
     }
 
-    public function create()
+    public function create(Category $category)
     {
-        return view('posts.create');
+        return view('posts.create')->with(['categories' => $category->get()]);
     }
 
     public function store(Post $post, PostRequest $request)
