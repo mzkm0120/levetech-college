4a97cad068b7744bbe8fe1897ccfd4b6b38018fe
mzkm0120 <mzkm0120@gmail.com>
2025-01-19 15:13:37 +0900

    curriculum 8-3, 8-4

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -7,22 +7,24 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 </head>
+<x-app-layout>
 
-<body>
-    <h1 class="title">編集画面</h1>
-    <div class="content">
-        <form action="/posts/{{ $post->id }}" method="POST">
-            @csrf
-            @method('PUT')
-            <div class='content__title'>
-                <h2>タイトル</h2>
-                <input type='text' name='post[title]' value="{{ $post->title }}">
-            </div>
-            <div class='content__body'>
-                <h2>本文</h2>
-                <input type='text' name='post[body]' value="{{ $post->body }}">
-            </div>
-            <input type="submit" value="保存">
-        </form>
-    </div>
-</body>
+    <body>
+        <h1 class="title">編集画面</h1>
+        <div class="content">
+            <form action="/posts/{{ $post->id }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class='content__title'>
+                    <h2>タイトル</h2>
+                    <input type='text' name='post[title]' value="{{ $post->title }}">
+                </div>
+                <div class='content__body'>
+                    <h2>本文</h2>
+                    <input type='text' name='post[body]' value="{{ $post->body }}">
+                </div>
+                <input type="submit" value="保存">
+            </form>
+        </div>
+    </body>
+</x-app-layout>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <title>Blog</title>
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-</head>
-
-<body>
+<x-app-layout>
     <h1>Blog Name</h1>
     <a href='/posts/create'>create</a>
     <div class='posts'>
@@ -17,6 +7,7 @@
             <h2 class='title'>
                 <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
             </h2>
+            {{ Auth::user()->name }}
             <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
             <p class='body'>{{ $post->body }}</p>
             <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
@@ -39,6 +30,13 @@
             }
         }
     </script>
-</body>
-
-</html>
+    <div>
+        @foreach($questions as $question)
+        <div>
+            <a href="https://teratail.com/questions/{{ $question['id'] }}">
+                {{ $question['title'] }}
+            </a>
+        </div>
+        @endforeach
+    </div>
+</x-app-layout>
